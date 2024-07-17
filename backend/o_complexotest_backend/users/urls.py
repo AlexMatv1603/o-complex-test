0840c34ee9e36412f2d347652b6e879c03b17169
Alexander Matveev <alexmatv1603@gmail.com>
2024-07-17 15:32:04 +0300

    Add login route

--- backend/o_complexotest_backend/users/urls.py
+++ backend/o_complexotest_backend/users/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from .views import (
     RegistrationAPIView,
+    LoginAPIView,
     MeAPIView,
 )
 
 
 urlpatterns = [
     path('register', RegistrationAPIView.as_view(), name='register'),
+    path('login', LoginAPIView.as_view(), name='login'),
+
     path('me', MeAPIView.as_view(), name='user-me'),
 ]
