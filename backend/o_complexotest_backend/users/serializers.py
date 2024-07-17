4440e08bd6064cd871fa62318905fdab97e7208c
Alexander Matveev <alexmatv1603@gmail.com>
2024-07-17 14:09:49 +0300

    Implement UserSerializer

--- backend/o_complexotest_backend/users/serializers.py
+++ backend/o_complexotest_backend/users/serializers.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
 
+from .models import UserModel
 
-class UserSerializer(serializers.Serializer):
-    pass
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = '__all__'
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'max_length': 128,
+                'min_length': 8,
+            }
+        }
