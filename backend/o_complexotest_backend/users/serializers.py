ec1950e3933144e9bfc223e4e1fd39ded201c90d
Alexander Matveev <alexmatv1603@gmail.com>
2024-07-17 14:50:34 +0300

    Implement create method for UserSerializer

--- backend/o_complexotest_backend/users/serializers.py
+++ backend/o_complexotest_backend/users/serializers.py
@@ -4,6 +4,14 @@ from .models import UserModel
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = UserModel(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
+
     class Meta:
         model = UserModel
         fields = '__all__'
